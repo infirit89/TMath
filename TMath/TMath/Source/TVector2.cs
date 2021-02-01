54ca20685154b2aacea250306a7079a2fd53340f
OKAY <george.dautov1235@gmail.com>
2021-02-01 22:23:24 +0200

    Added magnitude, cross and dot product to Vector 2 and Vector 3

--- TMath/TMath/Source/TVector2.cs
+++ TMath/TMath/Source/TVector2.cs
@@ -7,31 +7,70 @@ namespace TMath
     {
         public float X;
         public float Y;
+
+        public float this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return X;
+                    case 1:
+                        return Y;
+                    default:
+                        return 0;
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        X = value;
+                        break;
+                    case 1:
+                        Y = value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public readonly float magnitude;
         public TVector2(float x, float y)
         {
             X = x;
             Y = y;
+
+            magnitude = MathF.Sqrt(MathF.Pow(X, 2) * MathF.Pow(Y, 2));
         }
 
         public TVector2(int x, int y)
         {
             X = (int)x;
             Y = (int)y;
+
+            magnitude = MathF.Sqrt(MathF.Pow(X, 2) * MathF.Pow(Y, 2));
         }
 
         public TVector2(int value) 
         {
             X = value;
             Y = value;
+
+            magnitude = MathF.Sqrt(MathF.Pow(X, 2) * MathF.Pow(Y, 2));
         }
 
         public TVector2(float value) 
         {
             X = value;
             Y = value;
+
+            magnitude = MathF.Sqrt(MathF.Pow(X, 2) * MathF.Pow(Y, 2));
         }
 
-        public static float Dot(in TVector2 a, in TVector2 b) => (a.X * b.X) + (a.Y + b.Y);
 
         public static TVector2 Zero = new TVector2(0, 0);
         public static TVector2 One = new TVector2(1, 1);
@@ -40,6 +79,7 @@ namespace TMath
         public static TVector2 Down = new TVector2(0, -1);
         public static TVector2 Left = new TVector2(-1, 0);
         public static TVector2 Right = new TVector2(1, 0);
+        public static float Dot(in TVector2 a, in TVector2 b) => (a.X * b.X) + (a.Y * b.Y);
 
         public static bool operator ==(TVector2 a, TVector2 b) => ((a.X == b.X) && (a.Y == b.Y));
 
@@ -47,10 +87,15 @@ namespace TMath
 
         public static TVector2 operator +(TVector2 a) => a;
         public static TVector2 operator +(TVector2 a, TVector2 b) => new TVector2(a.X + b.X, a.Y + b.Y);
+
         public static TVector2 operator -(TVector2 a) => new TVector2(-a.X, -a.Y);
         public static TVector2 operator -(TVector2 a, TVector2 b) => new TVector2(a.X - b.X, a.Y - b.Y);
-        public static TVector2 operator *(TVector2 a, TVector2 b) => new TVector2(a.X * b.X, a.Y * b.Y);
-        public static TVector2 operator /(TVector2 a, TVector2 b) => new TVector2(a.X / b.X, a.Y / b.Y);
+
+        public static TVector2 operator *(TVector2 a, float b) => new TVector2(a.X * b, a.Y * b);
+        public static TVector2 operator *(TVector2 a, int b) => new TVector2(a.X * b, a.Y * b);
+
+        public static TVector2 operator /(TVector2 a, float b) => new TVector2(a.X / b, a.Y / b);
+        public static TVector2 operator /(TVector2 a, int b) => new TVector2(a.X / b, a.Y / b);
 
         public bool Equals(TVector2 other)
         {
--- TMath/TMath/Source/TVector3.cs
+++ TMath/TMath/Source/TVector3.cs
@@ -9,11 +9,50 @@ namespace TMath
         public float Y;
         public float Z;
 
+        public readonly float magnitude;
+
+        public float this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return X;
+                    case 1:
+                        return Y;
+                    case 2:
+                        return Z;
+                    default:
+                        return 0;
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        X = value;
+                        break;
+                    case 1:
+                        Y = value;
+                        break;
+                    case 2:
+                        Z = value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
         public TVector3(float x, float y, float z)
         {
             X = x;
             Y = y;
             Z = z;
+
+            magnitude = MathF.Sqrt(MathF.Pow(X, 2) * MathF.Pow(Y, 2) * MathF.Pow(Z, 2));
         }
 
         public TVector3(int x, int y, int z)
@@ -21,6 +60,26 @@ namespace TMath
             X = (int)x;
             Y = (int)y;
             Z = (int)z;
+
+            magnitude = MathF.Sqrt(MathF.Pow(X, 2) * MathF.Pow(Y, 2) * MathF.Pow(Z, 2));
+        }
+
+        public TVector3(float value) 
+        {
+            X = value;
+            Y = value;
+            Z = value;
+
+            magnitude = MathF.Sqrt(MathF.Pow(X, 2) * MathF.Pow(Y, 2) * MathF.Pow(Z, 2));
+        }
+
+        public TVector3(int value) 
+        {
+            X = (int)value;
+            Y = (int)value;
+            Z = (int)value;
+
+            magnitude = MathF.Sqrt(MathF.Pow(X, 2) * MathF.Pow(Y, 2) * MathF.Pow(Z, 2));
         }
 
         public static TVector3 Zero = new TVector3(0, 0, 0);
@@ -32,6 +91,14 @@ namespace TMath
         public static TVector3 Right = new TVector3(1, 0, 0);
 
         public static float Dot(in TVector3 a, in TVector3 b) => (a.X * b.X) + (a.Y * b.Y) + (a.Z * b.Z);
+        public static TVector3 Cross(in TVector3 a, in TVector3 b) 
+        {
+            float x, y, z;
+            x = a.Y * b.Z - b.Y * a.Z;
+            y = (a.X * b.Z - b.X * a.Z) * -1;
+            z = a.X * b.Y - b.X * a.Y;
+            return new TVector3(x, y, z);
+        }
 
         public static bool operator ==(TVector3 a, TVector3 b) => ((a.X == b.X) && (a.Y == b.Y) && (a.Z == b.Z));
 
